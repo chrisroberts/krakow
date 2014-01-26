c6fc27b8a87eebee850f6d542df39f3822fcec17
Chris Roberts <chrisroberts.code@gmail.com>
2014-01-26 07:48:08 -0800

    Add connection failure and reconnect support to producer

--- lib/krakow/producer.rb
+++ lib/krakow/producer.rb
@@ -4,6 +4,7 @@ module Krakow
     include Utils::Lazy
     include Celluloid
 
+    trap_exit  :connection_failure
     finalizer :goodbye_my_love!
 
     attr_reader :connection
@@ -11,13 +12,49 @@ module Krakow
     def initialize(args={})
       super
       required! :host, :port, :topic
-      @connection = Connection.new(:host => host, :port => port)
-      connection.init!
-      debug "Connection established: #{connection}"
+      optional :connect_retries
+      arguments[:reconnect_retries] ||= 10
+      arguments[:reconnect_interval] = 5
+      connect
+    end
+
+    def connect
+      info "Establishing connection to: #{host}:#{port}"
+      begin
+        @connection = Connection.new(:host => host, :port => port)
+        self.link connection
+        connection.init!
+        info "Connection established: #{connection}"
+      rescue => e
+        abort e
+      end
     end
 
     def to_s
       "<#{self.class.name}:#{object_id} {#{host}:#{port}} T:#{topic}>"
+    end
+
+    def connected?
+      connection && connection.alive?
+    end
+
+    def connection_failure(*args)
+      warn "Connection has failed to #{host}:#{port}"
+      retries = 0
+      begin
+        connect
+      rescue => e
+        retries += 1
+        warn "Connection retry #{retries}/#{reconnect_retries} failed. #{e.class}: #{e}"
+        if(retries < reconnect_retries)
+          sleep_interval = retries * reconnect_interval
+          debug "Sleeping for reconnect interval of #{sleep_interval} seconds"
+          sleep sleep_interval
+          retry
+        else
+          abort e
+        end
+      end
     end
 
     def goodbye_my_love!
@@ -32,24 +69,28 @@ module Krakow
     # message:: Message to send
     # Write message
     def write(*message)
-      if(message.size > 1)
-        debug 'Multiple message publish'
-        connection.transmit(
-          Command::Mpub.new(
-            :topic_name => topic,
-            :messages => message
+      if(connection.alive?)
+        if(message.size > 1)
+          debug 'Multiple message publish'
+          connection.transmit(
+            Command::Mpub.new(
+              :topic_name => topic,
+              :messages => message
+            )
           )
-        )
+        else
+          debug 'Single message publish'
+          connection.transmit(
+            Command::Pub.new(
+              :message => message.first,
+              :topic_name => topic
+            )
+          )
+        end
+        read(:validate)
       else
-        debug 'Single message publish'
-        connection.transmit(
-          Command::Pub.new(
-            :message => message.first,
-            :topic_name => topic
-          )
-        )
+        abort Error.new 'Remote connection is unavailable!'
       end
-      read(:validate)
     end
 
     # args:: Options (:validate)
