5ff481147b0ab379a7bcc7d563c7f3708647ccfe
Chris Roberts <chrisroberts.code@gmail.com>
2014-03-17 08:22:46 -0700

    Add support for configuring the ssl context used for tls upgraded connections

--- lib/krakow/connection_features/ssl.rb
+++ lib/krakow/connection_features/ssl.rb
@@ -8,7 +8,12 @@ module Krakow
         def initialize(io, args={})
           ssl_socket_arguments = [io]
           if(args[:ssl_context])
-            # ssl_socket_arguments << SSLContext.new
+            validate_ssl_args!(args[:ssl_context])
+            require 'openssl'
+            context = OpenSSL::SSL::SSLContext.new
+            context.cert = OpenSSL::X509::Certificate.new(File.open(args[:ssl_context][:certificate]))
+            context.key = OpenSSL::PKey::RSA.new(File.open(args[:ssl_context][:key]))
+            ssl_socket_arguments << context
           end
           @_socket = Celluloid::IO::SSLSocket.new(*ssl_socket_arguments)
           _socket.sync = true
@@ -25,6 +30,19 @@ module Krakow
             str << sysread(len - str.length)
           end
           str
+        end
+
+        private
+
+        def validate_ssl_args!(args)
+          [:key, :certificate].each do |arg_key|
+            unless(args.has_key?(arg_key))
+              raise ArgumentError.new "The `:ssl_context` option requires `#{arg_key.inspect}` to be set"
+            end
+            unless(File.readable?(args[arg_key]))
+              raise LoadError.new "Unable to read the `#{arg_key.inspect}` file from the `:ssl_context` arguments"
+            end
+          end
         end
 
       end
