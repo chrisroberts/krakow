a4975100697f0fc6e782fbb02bf58d2b8979f57c
Chris Roberts <chrisroberts.code@gmail.com>
2014-04-16 18:11:52 -0700

    Include a few more error types

--- lib/krakow/exceptions.rb
+++ lib/krakow/exceptions.rb
@@ -5,6 +5,8 @@ module Krakow
     class LookupFailed < Error; end
     class ConnectionFailure < Error; end
     class ConfigurationError < Error; end
+    class ConnectionUnavailable < Error; end
+    class OriginNotFound < Error; end
 
     class BadResponse < Error
       attr_accessor :result
