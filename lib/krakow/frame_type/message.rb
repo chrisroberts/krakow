2457ddfc0edce566b2f69cd3b3120e27beba7cdb
Brendan Schwartz <brendanschwartz@gmail.com>
2014-04-16 18:10:17 -0700

    Include message response methods on `Message` directly

--- lib/krakow/frame_type/message.rb
+++ lib/krakow/frame_type/message.rb
@@ -2,6 +2,8 @@ module Krakow
   class FrameType
     class Message < FrameType
 
+      attr_accessor :origin
+
       def initialize(args={})
         super
         required! :attempts, :timestamp, :message_id, :message
@@ -9,6 +11,27 @@ module Krakow
 
       def content
         message
+      end
+
+      def origin
+        unless(@origin)
+          error 'No origin has been specified for this message'
+          abort Krakow::Error::OriginNotFound.new('No origin specified for this message')
+        end
+        @origin
+      end
+
+      def confirm(*args)
+        origin.confirm(*[self, *args].compact)
+      end
+      alias_method :finish, :confirm
+
+      def requeue(*args)
+        origin.requeue(*[self, *args].compact)
+      end
+
+      def touch(*args)
+        origin.touch(*[self, *args].compact)
       end
 
     end
