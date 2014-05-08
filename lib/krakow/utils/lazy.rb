0d6dd05d3cd1610de51dc02292ffba9ffba80b49
Chris Roberts <chrisroberts.code@gmail.com>
2014-05-08 06:54:16 -0700

    Update lazy implementation to provide dsl. Remove instance method additions since it was just a hack and is slooow

--- lib/krakow/utils/lazy.rb
+++ lib/krakow/utils/lazy.rb
@@ -1,65 +1,105 @@
+require 'krakow'
+
 module Krakow
   module Utils
+    # Adds functionality to facilitate laziness
     module Lazy
 
       include Utils::Logging
 
-      attr_reader :arguments
+      # Instance methods for laziness
+      module InstanceMethods
 
-      def initialize(args={})
-        @arguments = {}.tap do |hash|
-          args.each do |k,v|
-            hash[k.to_sym] = v
-          end
-        end
-      end
-      alias_method :super_init, :initialize
+        # @return [Hash] argument hash
+        attr_reader :arguments
 
-      # args:: list of required keys
-      # Check that required keys exist in `arguments` hash. Raise
-      # error if not found
-      def required!(*args)
-        args.each do |key|
-          key = key.to_sym
-          unless(arguments.has_key?(key))
-            raise ArgumentError.new "Missing required option `#{key}`!"
-          end
-          define_singleton_method(key) do
-            arguments[key]
-          end
-          if(key.match(/\w$/))
-            define_singleton_method("#{key}?".to_sym) do
-              !!arguments[key]
+        # Create new instance
+        #
+        # @param args [Hash]
+        # @return [Object]
+        def initialize(args={})
+          @arguments = {}.tap do |hash|
+            self.class.properties.each do |name, options|
+              val = args[name]
+              if(options[:required] && !args.has_key?(name))
+                raise ArgumentError.new("Missing required option: `#{name}`")
+              end
+              if(val && options[:type] && !(valid = [options[:type]].flatten.compact).detect{|k| val.is_a?(k)})
+                raise TypeError.new("Invalid type for option `#{name}`. Valid - #{valid.map(&:to_s).join(',')}")
+              end
+              if(val.nil? && options[:default] && !args.has_key?(name))
+                val = options[:default].respond_to?(:call) ? options[:default].call : options[:default]
+              end
+              hash[name] = val
             end
           end
         end
+        alias_method :super_init, :initialize
+
+        # @return [String]
+        def to_s
+          "<#{self.class.name}:#{object_id}>"
+        end
+
+        # @return [String]
+        def inspect
+          "<#{self.class.name}:#{object_id} [#{arguments.inspect}]>"
+        end
+
       end
 
-      # args:: list of required keys
-      # Optional keys for arguments
-      def optional(*args)
-        args.each do |key|
-          key = key.to_sym
-          unless(arguments.has_key?(key))
-            arguments[key] = nil
+      # Class methods for laziness
+      module ClassMethods
+
+        # Add new property to class
+        #
+        # @param name [String]
+        # @param type [Class, Array<Class>]
+        # @param options [Hash]
+        # @option options [true, false] :required must be provided on initialization
+        # @option options [Object, Proc] :default default value
+        # @return [nil]
+        def property(name, type, options={})
+          name = name.to_sym
+          properties[name] = {:type => type}.merge(options)
+          define_method(name) do
+            arguments[name]
           end
-          define_singleton_method(key) do
-            arguments[key]
+          define_method("#{name}?") do
+            !!arguments[name]
           end
-          if(key.match(/\w$/))
-            define_singleton_method("#{key}?".to_sym) do
-              !!arguments[key]
-            end
+          nil
+        end
+
+        # Return properties
+        #
+        # @param args [Symbol] :required or :optional
+        # @return [Array<Hash>]
+        def properties(*args)
+          @properties ||= {}
+          if(args.include?(:required))
+            Hash[@properties.find_all{|k,v| v[:required]}]
+          elsif(args.include?(:optional))
+            Hash[@properties.find_all{|k,v| !v[:required]}]
+          else
+            @properties
           end
         end
+
       end
 
-      def to_s
-        "<#{self.class.name}:#{object_id}>"
-      end
+      class << self
 
-      def inspect
-        "<#{self.class.name}:#{object_id} [#{arguments.inspect}]>"
+        # Injects laziness into class
+        #
+        # @param klass [Class]
+        def included(klass)
+          klass.class_eval do
+            include InstanceMethods
+            extend ClassMethods
+          end
+        end
+
       end
 
     end
